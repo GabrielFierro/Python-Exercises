0d415e37d660e3c329f093f9d04bd2b079b50847
GabrielFierro <55300330+GabrielFierro@users.noreply.github.com>
2020-07-15 16:30:52 -0300

    Delete MostrarNumeros.py

--- For Loops/MostrarNumeros.py
+++
@@ -1,5 +0,0 @@
-#Algoritmo que utiliza la función range() para imprimir números hasta el rango indicado entre paréntesis
-# Por defecto, empieza a contar desde 0 y a su vez, incrementa en 1 la unidad del valor x
-
-for x in range(5):
-    print(x)
