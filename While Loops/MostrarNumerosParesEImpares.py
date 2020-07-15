97526aa6a8ad9acde1730a9c694888f1c1c290c2
GabrielFierro <55300330+GabrielFierro@users.noreply.github.com>
2020-07-15 16:29:59 -0300

    Delete MostrarNumerosParesEImpares.py

--- While Loops/MostrarNumerosParesEImpares.py
+++
@@ -1,9 +0,0 @@
-# Algoritmo que dada una variable inicializada con el valor cero, muestre que valores son pares y cuales impares.
-numero = 0
-
-while numero <= 5:
-    if numero % 2 == 0:
-        print("El número",numero,"es par")
-    else:
-        print("El número",numero,"es impar")
-    numero += 1
