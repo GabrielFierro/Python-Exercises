77acab4f86e802b99ca60f27f35b363346d01a93
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:56:40 -0300

    Delete ActualizarConjuntoDeFrutas.py

--- Sets/ActualizarConjuntoDeFrutas.py
+++
@@ -1,7 +0,0 @@
-# Algoritmo que añade varios elementos a un conjunto mediante el método update()
-
-colors = {"yellow", "blue", "green", "brown", "violet", "purple"}
-
-colors.update(["orange", "grey", "pink"])
-
-print(colors)
