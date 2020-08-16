436235fac7196aa7383a0deded34dff4948479fc
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:58:25 -0300

    Delete UnirConjuntos.py

--- Sets/UnirConjuntos.py
+++
@@ -1,9 +0,0 @@
-# Algoritmo que une dos conjuntos de elementos mediante el método union().
-
-animals1 = {"cow", "tiger", "bear", "monkey", "squirrel", "fox", "raccoon"}
-animals2 = {"dog", "kangaroo", "koala bear", "panda", "zebra"}
-
-animals3 = animals1.union(animals2) 
-# El método union() excluye los elementos duplicados y no establece un orden sobre el nuevo conjunto.
-
-print(animals3)
