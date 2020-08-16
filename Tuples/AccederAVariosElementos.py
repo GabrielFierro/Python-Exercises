8426b02ceabbdbb6ff2d29133cf6291e6de015af
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:46:23 -0300

    Delete AccederAVariosElementos.py

--- Tuples/AccederAVariosElementos.py
+++
@@ -1,7 +0,0 @@
-# Algoritmo que dada una tupla inicializada con valores, muestra por consola los elementos que posee dentro de un rango.
-
-this_tuple = ("apple", "banana", "cherry","kiwi", "melon","mango")
-
-# Acceder a varios ítems de la tupla con un rango de valores.
-
-print(this_tuple[2:5])
