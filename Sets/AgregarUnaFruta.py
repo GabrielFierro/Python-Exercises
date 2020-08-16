0a425ec165cdd1960ebdd2ee9f4aacdf1c1c5b51
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:56:47 -0300

    Delete AgregarUnaFruta.py

--- Sets/AgregarUnaFruta.py
+++
@@ -1,7 +0,0 @@
-# Algoritmo que añade un elemento a un conjunto mediante el método add()
-
-colors = {"yellow", "blue", "green", "brown", "violet", "purple"}
-
-colors.add("orange")
-
-print(colors)
