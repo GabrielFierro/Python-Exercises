10e585c1e5421983341b0776ffd8db991c9b9fb8
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:46:06 -0300

    Delete AccederAUnElemento2.py

--- Tuples/AccederAUnElemento2.py
+++
@@ -1,7 +0,0 @@
-# Algoritmo que dada una tupla inicializada con valores, muestra el primer elemento almacenado contando desde atrás hacia adelante.
-
-this_tuple = ("apple", "banana", "cherry","kiwi", "melon","mango")
-
-# Acceder a un ítem de la tupla con índice negativo.
-
-print(this_tuple[-1])
