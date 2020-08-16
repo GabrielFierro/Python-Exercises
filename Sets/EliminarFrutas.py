ac1ae1c3a1183baf6bd705ab07356db3ac748ebd
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:58:05 -0300

    Delete EliminarFrutas.py

--- Sets/EliminarFrutas.py
+++
@@ -1,7 +0,0 @@
-# Algoritmo que remueve todos los elementos dentro de un conjunto con el método clear().
-
-animals = {"cow", "tiger", "bear", "monkey", "squirrel", "fox", "raccoon"}
-
-animals.clear() # Método que elimina todos los elementos de un conjunto
-
-print(animals)
