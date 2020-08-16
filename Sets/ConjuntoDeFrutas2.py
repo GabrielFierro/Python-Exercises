b5f683d27af554a7721de23e19aaa7ee37a64252
Gabriel Fierro <55300330+GabrielFierro@users.noreply.github.com>
2020-08-15 22:57:15 -0300

    Delete ConjuntoDeFrutas2.py

--- Sets/ConjuntoDeFrutas2.py
+++
@@ -1,9 +0,0 @@
-# Algoritmo que dada una estructura de tipo set cargada con elementos de tipo 'str', recorre el conjunto mediante la estructura for y muestra todos sus elementos.
-
-fruits = {"apple", "banana", "cherry", "orange", "watermelon", "pear"}
-
-# Los conjuntos no tienen índice, por lo que para acceder a sus elementos puedo usar la estructura for.
-# "in" es una palabra reservada denominada "keyword", que pregunta si un valor específico se encuentra en el conjunto.
-
-for x in fruits: 
-    print(x)
