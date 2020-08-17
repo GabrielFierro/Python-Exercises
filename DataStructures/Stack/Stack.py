ae930bb3630da9514e594fce9911e1122cad6ac9
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2020-08-17 16:03:55 -0300

    Se actualizó el algoritmo y se agregaron comentarios

--- DataStructures/Stack/Stack.py
+++ DataStructures/Stack/Stack.py
@@ -5,10 +5,17 @@ una_pila = [2,4,6,8,10]
 
 # The operations can be implemented using the list methods such as append and pop.
 
-una_pila.append(12) # With the method append, allow to stack the element 12
+# With the method append, allow to stack the element 12
+una_pila.append(0) 
+una_pila.append(12) 
+una_pila.append(-1)
 
-print('The elements of stack: ' , una_pila)
+print('The elements of stack:',una_pila)
 
-una_pila.pop()#return the removed element
+print('Elements poped from the stack:')
 
-print('The elements of stack: ' , una_pila)
+una_pila.pop() # Deletes the top most element of the stack
+una_pila.pop() # Deletes the top most element of the stack
+una_pila.pop() # Deletes the top most element of the stack
+
+print('The elements of stack:',una_pila)
