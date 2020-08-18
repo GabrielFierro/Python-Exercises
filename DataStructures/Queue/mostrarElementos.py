0c11e84f5aeb3c2dcdfea82aaa885dfdf4d3700a
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2020-08-18 19:01:55 -0300

    Update

---
+++ DataStructures/Queue/mostrarElementos.py
@@ -0,0 +1,20 @@
+from collections import deque
+# Algoritmo que dada una estructura de datos de tipo Cola cargada con elementos, recorre la estructura y muestra sus elementos
+# A diferencia de la estructura de tipo Stack, se puede acceder tanto al frente como al final de tal estructura
+
+my_queue = deque(['red','blue','yellow','brown','green'])
+
+for x in my_queue:
+    print(x)
+
+# Accediendo al frente de la estructura
+print('El elemento del frente es:',my_queue.popleft())
+
+# Muestro los valores actuales
+print('Los valores actuales son:',my_queue)
+
+# Accediendo al final de la estructura
+print('El elemento del final de la estructura es:',my_queue.pop())
+
+# Muestro los valores actuales
+print('Los valores actuales son:',my_queue)
