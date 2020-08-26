9377a6a842eb61e870ec88c5fa7e3ec48a4bd1ed
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2020-08-26 11:59:24 -0300

    Update

---
+++ DataStructures/Lists/List.py
@@ -0,0 +1,43 @@
+my_list = []
+
+# Method append(): Adds an element at the end of the list
+
+my_list.append('red')
+my_list.append('green')
+my_list.append('blue')
+
+print(my_list)
+
+# Method pop(): Removes the last element 
+
+my_list.pop() 
+
+print(my_list)
+
+# Method remove(): Removes the first element with the specified value
+
+my_list.remove('green')
+
+print(my_list)
+
+# Method clear(): Removes all the elements 
+
+my_list.clear()
+
+print(my_list)
+
+# Method append(): Adds an element at the end of the list
+
+my_list.append('red')
+my_list.append('green')
+my_list.append('blue')
+
+# Method copy()
+
+new_list = []
+
+new_list = my_list.copy()
+
+print(my_list)
+print(new_list)
+
