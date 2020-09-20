d9b6bce5957901d0472502d59f1c41fcf48fef33
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2020-09-19 21:00:14 -0300

    Se actualizó el algoritmo

--- DataStructures/Lists/ElementosDePosPar.py
+++ DataStructures/Lists/ElementosDePosPar.py
@@ -1,4 +1,3 @@
-# List is a collection which is ordered and changeable. Allows duplicate members.
 # Una lista es una colección que está ordenada y es variable. Acepta elementos duplicados.
 
 # Algoritmo que dada una lista cargada con elementos, muestra los elementos que se encuentran en una posición par.
@@ -8,6 +7,6 @@ lista = [0, "hola", 1, "mundo", 3, ", bienvenidos"]
 i = 0
 
 while i < len(lista):
-    if i % 2 != 0:
+    if i % 2 == 0:
         print(lista[i])
     i+=1
