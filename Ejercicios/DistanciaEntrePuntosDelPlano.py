8796e5d385924b5d899f647402bb5cb52a7e9077
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2020-10-09 22:08:41 -0300

    Se eliminaron variables inicializadas

--- Ejercicios/DistanciaEntrePuntosDelPlano.py
+++ Ejercicios/DistanciaEntrePuntosDelPlano.py
@@ -2,10 +2,6 @@ import math
 # Algoritmo que le solicita al usuario que ingrese 2 pares ordenados de números pertenecientes al plano y calcula la distancia entre ambos.
 
 # Zona de inicialización de variables
-a = 0  # <=> x1
-b = 0  # <=> y1
-c = 0  # <=> x2
-d = 0  # <=> y2
 distancia = 0.0
 
 print("Ingrese el valor de la primer coordenada correspondiente al punto P")
